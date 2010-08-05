c2ac9efe75a3147cdf5c2e5b1548152dc851fa5b
Kelvin Luck <git@kelvinluck.com>
2010-08-05 14:11:28 +0100

    Added google analytics tracking to the demo site...

--- script/demo.js
+++ script/demo.js
@@ -18,3 +18,16 @@ $(function()
 		)
 	);
 });
+
+
+
+// Google analytics tracking code for demo site 
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-17828883-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
