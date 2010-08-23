054c58d61435b3943e6cf66fa1b943a5390b2537
Kelvin Luck <git@kelvinluck.com>
2010-08-23 21:12:23 +0100

    Updating mousewheel plugin to 3.0.3 which supports horizontal scrolling...

--- script/jquery.mousewheel.js
+++ script/jquery.mousewheel.js
@@ -3,9 +3,10 @@
  *
  * Thanks to: http://adomas.org/javascript-mouse-wheel/ for some pointers.
  * Thanks to: Mathias Bank(http://www.mathias-bank.de) for a scope bug fix.
+ * Thanks to: Seamus Leahy for adding deltaX and deltaY
  *
- * Version: 3.0.3-pre
- * 
+ * Version: 3.0.3
+ *
  * Requires: 1.2.2+
  */
 
@@ -15,19 +16,23 @@ var types = ['DOMMouseScroll', 'mousewheel'];
 
 $.event.special.mousewheel = {
     setup: function() {
-        if ( this.addEventListener )
-            for ( var i=types.length; i; )
+        if ( this.addEventListener ) {
+            for ( var i=types.length; i; ) {
                 this.addEventListener( types[--i], handler, false );
-        else
+            }
+        } else {
             this.onmousewheel = handler;
+        }
     },
-    
+
     teardown: function() {
-        if ( this.removeEventListener )
-            for ( var i=types.length; i; )
+        if ( this.removeEventListener ) {
+            for ( var i=types.length; i; ) {
                 this.removeEventListener( types[--i], handler, false );
-        else
+            }
+        } else {
             this.onmousewheel = null;
+        }
     }
 };
 
@@ -35,7 +40,7 @@ $.fn.extend({
     mousewheel: function(fn) {
         return fn ? this.bind("mousewheel", fn) : this.trigger("mousewheel");
     },
-    
+
     unmousewheel: function(fn) {
         return this.unbind("mousewheel", fn);
     }
@@ -43,16 +48,30 @@ $.fn.extend({
 
 
 function handler(event) {
-    var args = [].slice.call( arguments, 1 ), delta = 0, returnValue = true;
-    
+    var orgEvent = event, args = [].slice.call( arguments, 1 ), delta = 0, returnValue = true, deltaX = 0, deltaY = 0;
+
     event = $.event.fix(event || window.event);
     event.type = "mousewheel";
-    
-    if ( event.wheelDelta ) delta = event.wheelDelta/120;
-    if ( event.detail     ) delta = -event.detail/3;
-    
+
+    // Old school scrollwheel delta
+    if ( event.wheelDelta ) { delta = event.wheelDelta/120; }
+    if ( event.detail     ) { delta = -event.detail/3; }
+
+    // New school multidimensional scroll (touchpads) deltas
+    deltaY = delta;
+
+    // Gecko
+    if ( orgEvent.axis !== undefined && orgEvent.axis === orgEvent.HORIZONTAL_AXIS ) {
+        deltaY = 0;
+        deltaX = -1*delta;
+    }
+
+    // Webkit
+    if ( orgEvent.wheelDeltaY !== undefined ) { deltaY = orgEvent.wheelDeltaY/120; }
+    if ( orgEvent.wheelDeltaX !== undefined ) { deltaX = -1*orgEvent.wheelDeltaX/120; }
+
     // Add event and delta to the front of the arguments
-    args.unshift(event, delta);
+    args.unshift(event, delta, deltaX, deltaY);
 
     return $.event.handle.apply(this, args);
 }
