cf6004d578ba6e22df2ec562f64b576a2dd4658b
Kelvin Luck <git@kelvinluck.com>
2010-09-13 16:40:49 +0100

    Updating version numbers/ dates etc...

--- script/jquery.jscrollpane.js
+++ script/jquery.jscrollpane.js
@@ -1,5 +1,5 @@
 /*!
- * jScrollPane - v2.0.0beta3 - 2010-08-27
+ * jScrollPane - v2.0.0beta4 - 2010-09-13
  * http://jscrollpane.kelvinluck.com/
  *
  * Copyright (c) 2010 Kelvin Luck
@@ -8,7 +8,7 @@
 
 // Script: jScrollPane - cross browser customisable scrollbars
 //
-// *Version: 2.0.0beta3, Last updated: 2010-08-27*
+// *Version: 2.0.0beta4, Last updated: 2010-09-13*
 //
 // Project Home - http://jscrollpane.kelvinluck.com/
 // GitHub       - http://github.com/vitch/jScrollPane
@@ -39,6 +39,7 @@
 //
 // About: Release History
 //
+// 2.0.0beta4 - (2010-09-13) clickOnTrack support.
 // 2.0.0beta3 - (2010-08-27) Horizontal mousewheel, mwheelIntent, keyboard support, bug fixes
 // 2.0.0beta2 - (2010-08-21) Bug fixes
 // 2.0.0beta1 - (2010-08-17) Rewrite to follow modern best practices and enable horizontal scrolling, initially hidden
