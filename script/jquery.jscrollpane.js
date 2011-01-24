vitch/jScrollPane

commit b80165e3c5cb6d21f9569cacaffd55d33435d23c

Using focus rather than focusin to fix issue #34 - bug with nested scrollpanes and IE...
focusin was originally implemented in b821613d68f28236048d750c1cbf485502bfafc1 - everything seems to work with focus but not sure about ARIA enhanced widgets?

script/jquery.jscrollpane.js
@@ -829,8 +829,8 @@
 
 			function initFocusHandler()
 			{
-				pane.unbind('focusin.jsp').bind(
-					'focusin.jsp',
+				pane.unbind('focus.jsp').bind(
+					'focus.jsp',
 					function(e)
 					{
 						if(e.target === pane[0]){return;}
@@ -842,7 +842,7 @@
 			function removeFocusHandler()
 			{
 
-				pane.unbind('focusin.jsp');
+				pane.unbind('focus.jsp');
 			}
 			
 			function initKeyboardNav()

commit d3452edadd22083e15ed8220fcd77487a0d2f756

Fix for issue #43 - bug was introduced in b80165e3c5cb6d21f9569cacaffd55d33435d23c which was a fix of issue #34. Need to test in IE and make sure issue #34 hasn't regressed...

script/jquery.jscrollpane.js
@@ -57,7 +57,6 @@
 		// JScrollPane "class" - public methods are available through $('selector').data('jsp')
 		function JScrollPane(elem, s)
 		{
-
 			var settings, jsp = this, pane, paneWidth, paneHeight, container, contentWidth, contentHeight,
 				percentInViewH, percentInViewV, isScrollableV, isScrollableH, verticalDrag, dragMaxY,
 				verticalDragPosition, horizontalDrag, dragMaxX, horizontalDragPosition,
@@ -865,7 +864,7 @@
 
 			function initFocusHandler()
 			{
-				pane.unbind('focus.jsp').bind(
+				pane.find(':input,a').unbind('focus.jsp').bind(
 					'focus.jsp',
 					function(e)
 					{
@@ -878,7 +877,7 @@
 			function removeFocusHandler()
 			{
 
-				pane.unbind('focus.jsp');
+				pane.find(':input,a').unbind('focus.jsp');
 			}
 			
 			function initKeyboardNav()