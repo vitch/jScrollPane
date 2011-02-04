d67c370b60cc43f89cfdd8affd0cbb7a2b4aea91
Kelvin Luck <git@kelvinluck.com>
2011-02-04 14:09:32 +0000

    Updated header...

--- script/jquery.jscrollpane.js
+++ script/jquery.jscrollpane.js
@@ -1,5 +1,5 @@
 /*!
- * jScrollPane - v2.0.0beta9 - 2011-01-31
+ * jScrollPane - v2.0.0beta9 - 2011-02-04
  * http://jscrollpane.kelvinluck.com/
  *
  * Copyright (c) 2010 Kelvin Luck
@@ -8,7 +8,7 @@
 
 // Script: jScrollPane - cross browser customisable scrollbars
 //
-// *Version: 2.0.0beta9, Last updated: 2011-01-31*
+// *Version: 2.0.0beta10, Last updated: 2011-02-04*
 //
 // Project Home - http://jscrollpane.kelvinluck.com/
 // GitHub       - http://github.com/vitch/jScrollPane
@@ -39,6 +39,7 @@
 //
 // About: Release History
 //
+// 2.0.0beta10 - (in progress)
 // 2.0.0beta9 - (2011-01-31) new API methods, bug fixes and correct keyboard support for FF/OSX
 // 2.0.0beta8 - (2011-01-29) touchscreen support, improved keyboard support
 // 2.0.0beta7 - (2011-01-23) scroll speed consistent (thanks Aivo Paas)
