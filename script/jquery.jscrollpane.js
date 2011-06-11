172efa02c043b18bf064dca2ed22159906193ed3
Kelvin Luck <git@kelvinluck.com>
2011-06-11 15:57:39 +0100

    Updated date in the header...

--- script/jquery.jscrollpane.js
+++ script/jquery.jscrollpane.js
@@ -1,5 +1,5 @@
 /*!
- * jScrollPane - v2.0.0beta11 - 2011-06-09
+ * jScrollPane - v2.0.0beta11 - 2011-06-11
  * http://jscrollpane.kelvinluck.com/
  *
  * Copyright (c) 2010 Kelvin Luck
@@ -8,7 +8,7 @@
 
 // Script: jScrollPane - cross browser customisable scrollbars
 //
-// *Version: 2.0.0beta11, Last updated: 2011-06-09*
+// *Version: 2.0.0beta11, Last updated: 2011-06-11*
 //
 // Project Home - http://jscrollpane.kelvinluck.com/
 // GitHub       - http://github.com/vitch/jScrollPane
